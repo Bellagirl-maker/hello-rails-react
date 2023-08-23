a3f0132f7605be427c79c8ec1a9c1e4639829e0c
Isabella <isabellaotoo25@gmail.com>
2023-08-23 06:13:58 +0000

    Update controller and Slice

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  root 'root#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "root#index"
+
+  namespace :api do
+    namespace :v1 do
+      resources :greetings, only: ['index']
+    end
+  end
+
+  get '/greeting', to: 'root#index'
 end
